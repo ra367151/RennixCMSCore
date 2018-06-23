00aadc944a1fc2982bde61a70e260bd6e585634e
Rennix09 <Rennixtec@gmail.com>
2018-06-23 15:58:56 +0800

    预览页

---
+++ src/RennixCMS.Web/Controllers/PostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RennixCMS.Web.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("[controller]/[action]")]
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Preview(int id)
+        {
+            return View();
+        }
+    }
+}
