3bf152a94eb303911b9996a14d8e42c51923d3af
Rennix09 <Rennixtec@gmail.com>
2018-06-26 23:17:33 +0800

    markdown编辑器集成

---
+++ src/RennixCMS.Web/wwwroot/lib/editor.md/setting.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    window.content = null;
+
+    var editor = editormd("editormd", {
+        path: "/lib/editor.md/lib/",
+        height: 400,
+        width: "100%",
+        onchange: function () {
+           window.content = this.getMarkdown()||'';
+        }
+    });
+});
